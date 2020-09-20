bfd2b3dd520af19dabf42f1555c21a2964f47d08
Kirito <raokarthik83@gmail.com>
2020-09-20 15:40:38 +0530

    Added css to the navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,18 +2,20 @@
 import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css'
+//32a6d493
 const navbar = () => {
     return (
         <div>
-            <Navbar bg="light" expand="lg" variant="light">
-            <Navbar.Brand>KARTHIK</Navbar.Brand>
+            <Navbar expand="lg" className="navbarcustom" variant="dark">
+            <Navbar.Brand>&lt;KARTHIK/&gt;</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <Nav.Link>About</Nav.Link>
-                        <Nav.Link>Skills</Nav.Link>
-                        <Nav.Link>Experience</Nav.Link>
-                        <Nav.Link>Projects</Nav.Link>
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="ml-auto">
+                        <Nav.Link id="navitem">About</Nav.Link>
+                        <Nav.Link id="navitem">Skills</Nav.Link>
+                        <Nav.Link id="navitem">Experience</Nav.Link>
+                        <Nav.Link id="navitem">Projects</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
